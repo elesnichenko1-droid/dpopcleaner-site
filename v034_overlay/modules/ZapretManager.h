afcd55aaf51a214688666bf78ef5db31122df23e
elesnichenko1-droid <elesnichenko1@gmail.com>
2026-08-24 15:28:36 +0300

    feat: expand Zapret manager actions for 0.3.4

---
+++ v034_overlay/modules/ZapretManager.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <filesystem>
+#include <string>
+#include <vector>
+
+#include "modules/ZapretCenterModel.h"
+
+namespace dpop::zapret {
+
+struct Status {
+    bool serviceInstalled{};
+    bool serviceRunning{};
+    bool winwsRunning{};
+    bool bundleValid{};
+    std::filesystem::path bundleFolder;
+    std::filesystem::path missingBundleFile;
+};
+
+Status QueryStatus();
+std::vector<StrategyEntry> EnumerateStrategies();
+bool LaunchStrategy(const std::filesystem::path& relativeScript, std::wstring& error);
+bool LaunchDefaultStrategy(std::wstring& error);
+bool StopBundledWinws(std::wstring& error);
+bool OpenServiceManager(std::wstring& error);
+bool OpenBundledFolder(std::wstring& error);
+
+}
