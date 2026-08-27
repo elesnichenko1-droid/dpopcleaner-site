7b445add7827d5e88b3a14ed5f77d93bdd6bc77e
elesnichenko1-droid <elesnichenko1@gmail.com>
2026-08-27 11:01:56 +0300

    feat(0.4.18): add main entry point

---
+++ v0418/core/main.cpp
@@ -0,0 +1,7 @@
+#include "MainWindow.h"
+
+#include <windows.h>
+
+int WINAPI wWinMain(HINSTANCE instance, HINSTANCE, LPWSTR, int showCommand) {
+    return dpop0418::RunMainWindow(instance, showCommand);
+}
