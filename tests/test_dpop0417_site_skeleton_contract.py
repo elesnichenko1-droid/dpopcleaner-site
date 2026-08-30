6768a3a72d8ff1fd4d96867dde5fb4d6e0d0bf74
elesnichenko1-droid <elesnichenko1@gmail.com>
2026-08-30 14:38:56 +0300

    test: fix hero wording assertion

--- tests/test_dpop0417_site_skeleton_contract.py
+++ tests/test_dpop0417_site_skeleton_contract.py
@@ -33,7 +33,7 @@ class SiteSkeletonContractTests(unittest.TestCase):
         hero_end = index.index('</section>', hero_start)
         hero = index[hero_start:hero_end]
         for token in (
-            'чистее система',
+            'чище система',
             'очистка системы',
             'оперативной памяти',
             'zapret center',
