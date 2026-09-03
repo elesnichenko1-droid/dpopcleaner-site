5b9b804c86b02e2ece5cdeead025de4fc50f4f5d
elesnichenko1-droid <elesnichenko1@gmail.com>
2026-09-03 13:24:02 +0300

    fix(rev17): pin release manifest to rev17 tag

--- release-manifest.js
+++ release-manifest.js
@@ -3,14 +3,14 @@
   if(typeof module === 'object' && module.exports) module.exports = api;
   if(root) root.DPopReleaseManifest = api;
 })(typeof globalThis !== 'undefined' ? globalThis : this, function(){
-  const releaseUrl = /^https:\/\/github\.com\/elesnichenko1-droid\/dpopcleaner-site\/releases\/download\/v0\.4\.17-rev16\/DPopCleaner_Setup_0\.4\.17\.exe$/;
+  const releaseUrl = /^https:\/\/github\.com\/elesnichenko1-droid\/dpopcleaner-site\/releases\/download\/v0\.4\.17-rev17\/DPopCleaner_Setup_0\.4\.17\.exe$/;
 
   function isUsableManifest(m){
     return Boolean(
       m &&
       m.version === '0.4.17' &&
       m.channel === 'stable' &&
-      Number(m.revision) === 16 &&
+      Number(m.revision) === 17 &&
       m.available === true &&
       typeof m.download_url === 'string' &&
       releaseUrl.test(m.download_url) &&
